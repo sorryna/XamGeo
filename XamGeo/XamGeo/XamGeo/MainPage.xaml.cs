5f65dc5953dcd9d1b8f4cbef7c26e3dd6b13be8b
sikarin <so.sikarin@perfenterprise.com>
2021-01-14 16:26:07 +0700

    geolocation

---
+++ XamGeo/XamGeo/XamGeo/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamGeo
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public double Long { get; set; }
+        public double Lat { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void raise(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Long = location.Longitude;
+                    Lat = location.Latitude;
+                    raise("Long");
+                    raise("Lat");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+    }
+}
